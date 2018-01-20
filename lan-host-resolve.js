a134897c7b591b178e1d9f79873022fcb6d4bf92
alex <alex@rocketguys.com>
2018-01-20 20:48:07 +0300

    who-at-home3

---
+++ lan-host-resolve.js
@@ -0,0 +1,25 @@
+// Useful when no any local DNS
+
+const ATTRS = {
+  IP: 'ip',
+  MAC: 'mac',
+  HOSTNAME: 'hostname',
+};
+
+// @example [{"ip": "192.168.1.67", "hostname": "inv-phone", "mac": "58:E2:8F:58:CF:10"},,,]
+const registry = require('/home/pi/.config/lan-host-resolve.json');
+
+function detectArgType (arg) {
+  if (/^(?:\d+\.){3}\d+$/.test(arg)) return ATTRS.IP;
+  if (/^(?:\w+:){5}\w+$/.test(arg)) return ATTRS.MAC;
+  return ATTRS.HOSTNAME;
+}
+
+function resolve(arg, target = ATTRS.IP) {
+  const arg_ = arg.trim().toLowerCase();
+  const argType = detectArgType(arg_);
+  const device =  registry.find(record => record[argType].toLowerCase() === arg_)
+  return device[target];
+}
+
+module.exports = { ATTRS, detectArgType, registry, resolve };
